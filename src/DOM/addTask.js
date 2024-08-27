e47b26b8fdb7217def6b3f4e2b20f40d6444e761
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-27 17:05:05 +0000

    add task ui

--- src/DOM/addTask.js
+++ src/DOM/addTask.js
@@ -5,30 +5,28 @@ export const addTask = () => {
   formDiv.style.display = 'flex';
   const form = `
     <div class="form">
-      <input type="text" name="title" class="title" placeholder="Title">
+      <input type="text" name="title" class="title" placeholder="Title" required>
       
-      <textarea class="desc" placeholder="Description" rows="10"></textarea>
+      <textarea class="desc" placeholder="Description" rows="10" name="desc"></textarea>
       
-      <label for="date">Date:</label>
-      <input type="date" name="date" class="date">
-      
-      <label for="priority">Priority</label>
-      <select class="priority">
-        <option value="1">Low</option>
-        <option value="2">Medium</option>
-        <option value="3">High</option>
-      </select>
-
-      <input class="priority" name="priority" type="radio">Low</input>
-      <input class="priority" name="priority" type="radio">Medium</input>
-      <input class="priority" name="priority" type="radio">High</input>
+      <div class='radioElements'>
+      <input type="date" name="date" class="date" required>
+      <div class='subRadio'>
+      <input id="low" class="priority" name="priority" type="radio" value="low" required></input>
+      <label for="low" class="labelPriority lLow">LOW</label>
+      <input id="medium" class="priority" name="priority" type="radio" value="medium" required></input>
+      <label for="medium" class="labelPriority lMedium">MEDIUM</label>
+      <input id="high" class="priority" name="priority" type="radio" value="high" required></input>
+      <label for="high" class="labelPriority lHigh">HIGH</label>
+      </div>
+      </div>
 
       
-      <label for="list">List</label>
-      <select class="list">
+      <label for="listId">List</label>
+      <select id="listId" class="list" name="list">
       </select>
       
-      <button type="button">Submit</button> <!-- Use button type="button" if not using a form -->
+      <button type="button">Submit</button>
       <img src="${cross}" class="formCross">
     </div>
   `;
@@ -41,9 +39,14 @@ export const addTask = () => {
   const closeFunc = () => {
     formDiv.style.display = 'none';
   }
-  formDiv.addEventListener("click",closeFunc);
-  modal.addEventListener("click",(event) => {
+  formDiv.addEventListener("click", closeFunc);
+  modal.addEventListener("click", (event) => {
     event.stopPropagation();
-  } );
+  });
   close.addEventListener("click", closeFunc);
-}
+}      //<label for="priority">Priority</label>
+//<select class="priority">
+//  <option value="1">Low</option>
+//  <option value="2">Medium</option>
+//  <option value="3">High</option>
+//</select>
